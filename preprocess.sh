0f5d772807228f72e66da18f1f5530288fd1cead
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2016-02-15 22:09:35 +0200

    Cleaning pipeline in a bash script

--- preprocess.sh
+++ preprocess.sh
@@ -1,5 +1,13 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -x
 
-./clean.pl Kangyur_Lhasa/*/* TENGYUR_ACIP/*/*/*
-./tokenize.pl Kangyur_Lhasa/*/* TENGYUR_ACIP/*/*/*
-./enumerate.pl Kangyur_Lhasa/*/* TENGYUR_ACIP/*/*/* >mapped.txt
+DIR=$(dirname $0)
+rm -rf clean/*
+cp -r Kangyur_Lhasa TENGYUR_ACIP clean/
+"$DIR"/clean.pl clean/Kangyur_Lhasa/*/* clean/TENGYUR_ACIP/*/*/*
+rm -rf tokenized/*
+cp -r clean/* tokenized/
+"$DIR"/tokenize.pl tokenized/Kangyur_Lhasa/*/* tokenized/TENGYUR_ACIP/*/*/*
+rm -rf enumerated/*
+cp -r tokenized/* enumerated/
+"$DIR"/enumerate.pl enumerated/Kangyur_Lhasa/*/* enumerated/TENGYUR_ACIP/*/*/*
+echo done
