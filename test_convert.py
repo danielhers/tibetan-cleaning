e07397ad394b60b805bd2860619929e6f8e0d9b6
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2016-02-17 11:27:19 +0200

    Remove output after testing

--- test_convert.py
+++ test_convert.py
@@ -1,18 +1,22 @@
 import unittest
 
+from os import unlink
+
 from convert_matches_to_raw import convert_matches
 
 
 class ConversionTest(unittest.TestCase):
     def test_convert_matches_to_raw(self):
+        raw_matches_csv = "test_files/raw_matches.csv"
         convert_matches("test_files/clean_matches.csv",
                         ("test_files/raw1.txt", "test_files/raw2.txt"),
-                        "test_files/raw_matches.csv")
-        with open("test_files/raw_matches.csv") as f:
+                        raw_matches_csv)
+        with open(raw_matches_csv) as f:
             res = f.readlines()
         with open("test_files/raw_matches_ref.csv") as f:
             ref = f.readlines()
         self.assertSequenceEqual(ref, res)
+        unlink(raw_matches_csv)
 
 
 if __name__ == '__main__':
