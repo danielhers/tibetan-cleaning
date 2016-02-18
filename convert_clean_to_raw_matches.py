3579a693a338d8b553e021097c5273f280b46801
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2016-02-18 11:52:52 +0200

    Tests

---
+++ convert_clean_to_raw_matches.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import argparse
+import csv
+
+
+def convert_matches(in_matches, mapping, out_matches):
+    with open(in_matches) as f:
+        clean_matches = sorted(list(map(tuple, csv.reader(f))))
+
+    with open(mapping[0]) as f:
+        mapping1 = [(int(c), int(r)) for c, r in csv.reader(f)]
+
+    with open(mapping[1]) as f:
+        mapping2 = [(int(c), int(r)) for c, r in csv.reader(f)]
+
+    raw_matches = []
+    for clean_start1, clean_start2, clean_end1, clean_end2, score in clean_matches:
+        clean_start1, clean_start2, clean_end1, clean_end2 = map(
+            int, (clean_start1, clean_start2, clean_end1, clean_end2))
+        raw_start1 = next(r for c, r in reversed(mapping1) if c <= clean_start1)
+        raw_start2 = next(r for c, r in reversed(mapping2) if c <= clean_start2)
+        raw_end1 = next(r for c, r in mapping1 if c >= clean_end1)
+        raw_end2 = next(r for c, r in mapping2 if c >= clean_end2)
+        raw_matches.append(map(str, (raw_start1, raw_start2, raw_end1, raw_end2)))
+
+    with open(out_matches, "w") as f:
+        f.writelines(",".join(m) + "\n" for m in raw_matches)
+
+
+def main():
+    argparser = argparse.ArgumentParser(description="Convert a matches file with clean syllable spans "
+                                                    "into a matches file with raw character spans.")
+    argparser.add_argument("in_matches", help="CSV file with clean syllable spans")
+    argparser.add_argument("mapping", nargs=2, help="CSV file with clean to raw mapping")
+    argparser.add_argument("-o", "--out_matches", default="-",
+                           help="output CSV file with raw character spans")
+    args = argparser.parse_args()
+    convert_matches(args.in_matches, args.mapping, args.out_matches)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_convert.py
@@ -0,0 +1,59 @@
+import unittest
+from os import unlink
+
+import clean_and_map
+import convert_clean_to_raw_matches
+import convert_syllable_to_char_matches
+
+
+class ConversionTest(unittest.TestCase):
+    def test_clean_and_map(self):
+        for i in (1, 2):
+            clean_file = "test_files/clean%d.txt" % i
+            map_file = "test_files/map%d.txt" % i
+            clean_and_map.clean_and_map(
+                "test_files/raw%d.txt" % i,
+                clean_file,
+                map_file)
+            with open(clean_file) as f:
+                res_clean = f.readlines()
+            with open("test_files/clean%d_ref.txt" % i) as f:
+                ref_clean = f.readlines()
+            self.assertSequenceEqual(ref_clean, res_clean)
+            unlink(clean_file)
+            with open(map_file) as f:
+                res_map = f.readlines()
+            with open("test_files/map%d_ref.txt" % i) as f:
+                ref_map = f.readlines()
+            self.assertSequenceEqual(ref_map, res_map)
+            unlink(map_file)
+
+    def test_convert_syllable_to_char_matches(self):
+        raw_matches_csv = "test_files/char_matches.csv"
+        convert_syllable_to_char_matches.convert_matches(
+            "test_files/syllable_matches.csv",
+            ("test_files/raw1.txt", "test_files/raw2.txt"),
+            raw_matches_csv)
+        with open(raw_matches_csv) as f:
+            res = f.readlines()
+        with open("test_files/char_matches_ref.csv") as f:
+            ref = f.readlines()
+        self.assertSequenceEqual(ref, res)
+        unlink(raw_matches_csv)
+
+    def test_convert_clean_to_raw_matches(self):
+        raw_matches_csv = "test_files/raw_matches.csv"
+        convert_clean_to_raw_matches.convert_matches(
+            "test_files/clean_matches.csv",
+            ("test_files/map1_ref.txt", "test_files/map1_ref.txt"),
+            raw_matches_csv)
+        with open(raw_matches_csv) as f:
+            res = f.readlines()
+        with open("test_files/char_matches_ref.csv") as f:
+            ref = f.readlines()
+        self.assertSequenceEqual(ref, res)
+        unlink(raw_matches_csv)
+
+
+if __name__ == '__main__':
+    unittest.main()
