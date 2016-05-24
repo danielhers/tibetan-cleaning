7a9696fceae6911bfc504e9b552396674038304b
Dani <daniel.labenski@gmail.com>
2016-05-24 13:35:31 +0300

    rewrite convert_syllable_to_char_matches

--- convert_syllable_to_char_matches.py
+++ convert_syllable_to_char_matches.py
@@ -3,73 +3,41 @@ import argparse
 import csv
 from operator import itemgetter
 
-"""
-Example:
-in_matches:
-0,2,1,3,1.0
-1,5,1,5,0.9
+def get_syllable_to_char_map(txt):
+    tokens = txt.split(' ')
+    token_start = [None]*len(tokens)
+    token_end =[None]*len(tokens)
+    token_start[0] = 0
+    token_end[0] = len(tokens[0])
+    for i in range(1,len(tokens)):
+        token_start[i] = token_end[i-1] + 1
+        token_end[i] = token_start[i] + len(tokens[i])
 
-raw[1]:
-bsgrubs grub drup gup shup
-
-raw[2]:
-par gpar spar dpar bar
-
-out_matches:
-0,12,4,13,1.0
-8,26,4,22,0.9
-"""
-
+    return (token_start , token_end)
 
 def convert_matches(in_matches, raw_files, out_matches):
-    with open(in_matches) as f:
-        reader = csv.reader(f)
-        #clean_matches = sorted(list(map(tuple, reader)))
-        clean_matches = list(map(tuple, reader))
 
     raw_text = []
     for file in raw_files:
         with open(file) as f:
             raw_text.append(f.read())
 
-    syllables = tuple(map(str.split, raw_text))
-    raw_matches = [[], []]
+    file_mapping = []
+    for txt in raw_text:
+        file_mapping.append(get_syllable_to_char_map(txt))
 
-    # 0: txt 1 start, 1: txt 2 start, 2: txt 1 end, 3: txt 2 end
-    clean_spans = [[], [], [], []]
-    clean_spans[0], clean_spans[1], clean_spans[2], clean_spans[3], scores = zip(*clean_matches)
-    for i in range(0, 4):
-        clean_spans[i] = [int(x) for x in clean_spans[i]]
-
-    (indices_txt2, clean_spans[1]) = zip(*sorted(enumerate(clean_spans[1]), key=itemgetter(1)))
-    clean_spans[3] = [clean_spans[3][i] for i in indices_txt2]
-    for i in (0, 1):
-        raw_start = 0
-        clean_pos = 0
-        for match_num in range(0, len(clean_spans[0])):
-            raw_start += sum(len(s) + 1 for s in syllables[i][clean_pos:clean_spans[i][match_num]])
-            clean_pos = clean_spans[i][match_num]
-            raw_end = raw_start + sum(
-                len(s) + 1 for s in syllables[i][clean_pos:clean_spans[2 + i][match_num]]) - 1
-            raw_matches[i].append((raw_start, raw_end))
-
-    temp_raw_matches = [None] * len(raw_matches[1])
-    for i in range(0, len(raw_matches[1])):
-        temp_raw_matches[indices_txt2[i]] = raw_matches[1][i]
-    raw_matches[1] = temp_raw_matches
-
-    if out_matches == "-":
-        for match_num in range(0, len(clean_spans[0])):
-            print(str(raw_matches[0][match_num][0]) + ',' + str(
-                raw_matches[1][match_num][0]) + ',' + str(raw_matches[0][match_num][1]) + ',' + str(
-                raw_matches[1][match_num][1]) + ',' + scores[match_num] + '\n')
-    else:
-        with open(out_matches, "w") as f:
-            # writer = csv.writer(f)
-            for match_num in range(0, len(clean_spans[0])):
-                f.write(str(raw_matches[0][match_num][0]) + ',' + str(
-                    raw_matches[1][match_num][0]) + ',' + str(raw_matches[0][match_num][1]) + ',' + str(
-                    raw_matches[1][match_num][1]) + ',' + scores[match_num] + '\n')
+    with open(in_matches) as fin:
+        reader = csv.reader(fin)
+        with open(out_matches, "wb") as fout:
+            writer = csv.writer(fout)
+            for line in reader:
+                writer.writerow([
+                    str(file_mapping[0][0][int(line[0])]),
+                    str(file_mapping[1][0][int(line[1])]),
+                    str(file_mapping[0][1][int(line[2])]),
+                    str(file_mapping[1][1][int(line[3])]),
+                    line[4]
+                ])
 
 
 def main():
--- syllable_matches_to_txt_csv.py
+++ syllable_matches_to_txt_csv.py
@@ -3,7 +3,6 @@ import convert_syllable_to_char_matches as syl_to_char
 import matches_to_txt_csv as matches_to_txt
 
 # todo: call syllable_to_txt
-#syllable_matches_filename = r'C:\Results\Tibet\test_tf_idf_ranking.union.txt'
 syllable_matches_filename = r'C:\Results\Tibet\1.txt'
 article1_filename = r'C:\DataSets\Tibet\Kangyur_Tenjur-CLEAN\stem\Kangyur_Lhasa\001\acip-k_lha_sa-001-001.txt'
 article2_filename = r'C:\DataSets\Tibet\Kangyur_Tenjur-CLEAN\stem\TENGYUR_ACIP\01_BSTOD_TSOGS\001_KA\TD1110E.txt'
@@ -12,7 +11,7 @@ char_matches_filename = syllable_matches_filename + r'_chars'
 syl_to_char.convert_matches(syllable_matches_filename, [article1_filename , article2_filename ], char_matches_filename)
 
 # TODO: call sort match
-#sorter.sort_matches(char_matches_filename)
+sorter.sort_matches(char_matches_filename)
 
 # todo: call matches_to_txt
 txt_matches_filename = syllable_matches_filename + '_text.csv'
